b291027f61a60e4137e3df029b464726a3598705
vineethvarma98 <vineeth67z@gmail.com>
2026-02-18 16:22:04 -0500

    file upload to s3

---
+++ s3_1.py
@@ -0,0 +1,28 @@
+import boto3
+import os
+
+s3 = boto3.client('s3')
+
+bucket_name = "dataengpracticebucket"
+file_path_directory = "E:/VARMA/data science/countries_dataset/countries_dataset/csv_data/countries_population/"
+
+
+# s3.upload_file(file_path,bucket_name,"testing.csv")
+
+for files in os.listdir(file_path_directory):
+    file_path = os.path.join(file_path_directory, files)
+    file_type = files.split(".")[1]
+    #print(file_type)
+    #print(file_path)
+    if file_type == "csv":
+        s3.upload_file(file_path,bucket_name,f"csv/{files}")
+    elif file_type == "json":
+        s3.upload_file(file_path,bucket_name,f"json/{files}")
+    elif file_type == "orc":
+        s3.upload_file(file_path,bucket_name,f"orc/{files}")
+    elif file_type == "parquet":
+        s3.upload_file(file_path,bucket_name,f"parquet/{files}")
+    else:
+        print("no files found")
+
+print("done")
